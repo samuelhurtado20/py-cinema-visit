91829b1bf8d9af1d8fb571ad9d8d854ec2507e2d
Samuel Hurtado <samuelhurtado20@hotmail.com>
2026-03-09 18:19:48 -0500

    Refactor cinema management and add cinema_visit function
    Refactored cinema management code to separate classes into different modules and added a cinema_visit function to handle the visit process.

--- app/main.py
+++ app/main.py
@@ -1,33 +1,27 @@
-class CinemaBar:
-    @staticmethod
-    def sell_product(product: str, customer: "Customer") -> None:
-        print(f"Cinema bar sold {product} to {customer.name}.")
-    
-class CinemaHall:
-    def __init__(self, hall_number: int):
-        self.hall_number = hall_number
-    
-    def movie_session(self, 
-                      movie_name: str, 
-                      customers: list["Customer"], 
-                      cleaning_staff: "Cleaner") -> None:
-        print(f'"{movie_name}" started in hall number {self.hall_number}.')
-        for customer in customers:
-            customer.watch_movie(movie_name)
-        print(f'"{movie_name}" ended.')
-        cleaning_staff.clean_hall(self.hall_number)
-    
-class Customer:
-    def __init__(self, name: str, food: str):
-        self.name = name
-        self.food = food
-    
-    def watch_movie(self, movie: str) -> None:
-        print(f'{self.name} is watching "{movie}".')
-        
-class Cleaner:
-    def __init__(self, name: str):
-        self.name = name
-    
-    def clean_hall(self, hall_number: int) -> None:
-        print(f'Cleaner {self.name} is cleaning hall number {hall_number}.')
+from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+
+
+def cinema_visit(
+    customers: list, 
+    hall_number: int, 
+    cleaner: str, 
+    movie: str
+) -> None:
+    customer_instances = [
+        Customer(name=c.get("name"), food=c.get("food")) 
+        for c in customers
+    ]
+    cleaning_staff = Cleaner(name=cleaner)
+    hall = CinemaHall(hall_number=hall_number)
+
+    for customer in customer_instances:
+        CinemaBar.sell_product(product=customer.food, customer=customer)
+
+    hall.movie_session(
+        movie_name=movie, 
+        customers=customer_instances, 
+        cleaning_staff=cleaning_staff
+    )
