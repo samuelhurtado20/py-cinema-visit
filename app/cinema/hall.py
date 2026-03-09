136792d03799ef33f2565cf6dfe86224ff2fc2b9
Samuel Hurtado <samuelhurtado20@hotmail.com>
2026-03-09 18:16:20 -0500

    Add movie session method to CinemaHall class

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -1,3 +1,13 @@
 class CinemaHall:
     def __init__(self, hall_number: int):
         self.hall_number = hall_number
+    
+    def movie_session(self, 
+                      movie_name: str, 
+                      customers: list["Customer"], 
+                      cleaning_staff: "Cleaner") -> None:
+        print(f'"{movie_name}" started in hall number {self.hall_number}.')
+        for customer in customers:
+            customer.watch_movie(movie_name)
+        print(f'"{movie_name}" ended.')
+        cleaning_staff.clean_hall(self.hall_number)
